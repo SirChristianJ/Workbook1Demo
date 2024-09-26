100100a4a0d580dd1329b27418a56a428029bb6d
SirChristianJ <git@github.com:SirChristianJ/todoApp.git>
2024-09-26 15:57:32 -0400

    Added calculations + greeting!

--- src/main/java/com/pluralsight/Demo1.java
+++ src/main/java/com/pluralsight/Demo1.java
@@ -3,5 +3,19 @@ package com.pluralsight;
 public class Demo1 {
     public static void main(String[] args){
         System.out.println("Hello" + " " + "Werlda");
+
+        String name = "Christian";
+        String greeting = "Hello " + name;
+        System.out.println(greeting);
+
+        int numberOfDaysWatchingTV = 5;
+        int minutesPerDayWatchingTV = 70;
+        int minutesPerWeekWatchingTV = numberOfDaysWatchingTV * minutesPerDayWatchingTV;
+        int minutesPerYearWatchingTV = minutesPerWeekWatchingTV * 52;
+        int hoursPerYearWatchingTV = minutesPerYearWatchingTV/60;
+        int hoursInWorkWeek = 35;
+        int workWeeksPerYearWatchingTV = hoursPerYearWatchingTV / hoursInWorkWeek;
+
+        System.out.println(name + ", you spend " + workWeeksPerYearWatchingTV + " hours per week watching TV... watch more!");
     }
 }
